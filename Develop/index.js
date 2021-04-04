f6f9387f1654df317ccae85b92dcf336081230f0
michael oreilly <michaelheartbound99@hotmail.com>
2021-04-03 21:48:37 -0600

    added const

---
+++ Develop/index.js
@@ -0,0 +1,5 @@
+const Manager = require("./lib/Manager");
+const Engineer = require(".lib/Engineer");
+const Intern = require("./lib/Intern");
+const path = require("path");
+const fs = require("fs");
