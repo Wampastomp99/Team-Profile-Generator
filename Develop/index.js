8c1263ed9000e05fc0180c291bc3056d44e8f52f
michael oreilly <michaelheartbound99@hotmail.com>
2021-04-04 10:29:28 -0600

    added intern and more java

--- Develop/index.js
+++ Develop/index.js
@@ -3,3 +3,10 @@ const Engineer = require(".lib/Engineer");
 const Intern = require("./lib/Intern");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
